98c88f3d7f45043e89f56b131227036a9af0acce
stef <stefan@stefans-macbook-pro.local>
2009-06-24 14:38:10 -0500

    test case

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,7 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Group do
   it "should create a new instance given valid attributes" do
     user = User.create(:login => 'tester')
-    group1 = Group.create(:name => 'group1')
-    user.groups.create(:name => 'group1').should_not be_valid
+    group1 = user.groups.create(:name => 'group1')
+  end
+
+  it "should not raise a validation error on collect#create" do
+    @user = User.create(:login => 'tester')
+    @group1 = user.groups.create(:name => 'group1')
+    @user.groups.create(:name => @group1.name).should_not be_valid
   end
 end
